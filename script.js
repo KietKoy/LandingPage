97d9bc0991e29cbcdf73bb6d47cda69c38e97c1b
KietKoy <truongtuankiet@KietKoy.local>
2023-01-12 23:16:13 +0700

    done JS

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const btnDisplaySearch = document.querySelector('.search');
 let isSearch = true;
 btnDisplaySearch.addEventListener('click', () => {
     if(isSearch === true) {
-        const iconSearch = document.querySelector('.fa-magnifying-glass');
-        const iconNotSearch = document.querySelector('.fa-xmark');
+        const iconSearch = document.querySelector('.js-icon-search-header');
+        const iconNotSearch = document.querySelector('.js-icon-notsearch');
         const formSearch = document.querySelector('.form-search');
         iconSearch.setAttribute('style', 'opacity: 0');
         iconNotSearch.setAttribute('style', 'opacity: 1');
@@ -11,8 +11,8 @@ btnDisplaySearch.addEventListener('click', () => {
         isSearch = false;
     }
     else {
-        const iconSearch = document.querySelector('.fa-magnifying-glass');
-        const iconNotSearch = document.querySelector('.fa-xmark');
+        const iconSearch = document.querySelector('.js-icon-search-header');
+        const iconNotSearch = document.querySelector('.js-icon-notsearch');
         const formSearch = document.querySelector('.form-search');
         iconSearch.setAttribute('style', 'opacity: 1');
         iconNotSearch.setAttribute('style', 'opacity: 0');
@@ -51,3 +51,17 @@ btnExitNavMobile.addEventListener('click', () => {
     const navMobile = document.querySelector('.nav-mobile');
     navMobile.setAttribute('style', 'transform: translateX(-100%);');
 });
+const btnChooseLanguage = document.querySelector('.btn-choose-language');
+btnChooseLanguage.addEventListener('click', () => {
+    const languageOptionsDropdown = document.querySelector('.language-options-mobile-dropdown');
+    languageOptionsDropdown.setAttribute('style', 'transform: translateY(0)');
+    const navMobileFirst = document.querySelector('.nav-mobile-first');
+    navMobileFirst.setAttribute('style', 'transform: translateY(200%)');
+})
+const btnExitLanguageOptions = document.querySelector('.btn-exit-language-options');
+btnExitLanguageOptions.addEventListener('click', () => {
+    const languageOptionsDropdown = document.querySelector('.language-options-mobile-dropdown');
+    languageOptionsDropdown.setAttribute('style', 'transform: translateY(100%)');
+    const navMobileFirst = document.querySelector('.nav-mobile-first');
+    navMobileFirst.setAttribute('style', 'transform: translateY(0)');
+})
